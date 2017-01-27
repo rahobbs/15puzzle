e3e72233ea676874570d827dc0006dbd9e0be367
rachael <hobbs.rachael@gmail.com>
2017-01-27 17:58:25 -0500

    logic working for moving tiles within a column, buggy if empty space is clicked

--- game.js
+++ game.js
@@ -17,9 +17,10 @@ var game = {
         td.appendChild(textNode);
         //If the cell has a tile, it gets a background color
         if (this.gameState[i][j] !== null) {
-          td.style.backgroundColor = '#2edeed'
+          td.style.backgroundColor = '#2edeed';
         //If a cell has no tile, it gets no text
         } else {
+          td.style.background = "#fff";
           td.removeChild(td.childNodes[0]);
         }
       }
@@ -27,36 +28,61 @@ var game = {
   },
   moveTile: function() {
     // if target is in a row or column with null, move as appropriate
-    var rowId = this.getAttribute('id').split('-')[0]
-    var colId = this.getAttribute('id').split('-')[1];
+    var rowId = parseInt(this.getAttribute('id').split('-')[0]);
+    var colId = parseInt(this.getAttribute('id').split('-')[1]);
     var rowOfClick = game.gameState[rowId];
     var indexOfNull = rowOfClick.indexOf(null);
     var indexOfClick = rowOfClick.indexOf(parseInt(this.textContent));
 
-
     //If null square is in the same row as the click
     if (indexOfNull !== -1) {
-      if (indexOfClick < indexOfNull) {
-        //What was null is set to the value of clicked
-        rowOfClick.splice(indexOfClick, 0, null);
-        rowOfClick.splice(indexOfNull + 1, 1);
-      } else if (indexOfClick > indexOfNull) {
-        rowOfClick.splice(indexOfNull, 1);
-        rowOfClick.splice(indexOfClick, 0, null);
-      }
+      game.shiftInRow(indexOfClick, indexOfNull, rowOfClick);
     //If the null square is in the same column as the click
     } else {
-      for (var i in game.gameState) {
-        for (var j in game.gameState) {
-          if (game.gameState[i][colId] === null) {
-            window.alert("column contains null!")
-            break;
-          }
-        }
-      }
+      game.shiftInColumn(rowId, colId, rowOfClick);
     }
     console.log(game.gameState);
     game.setupBoard();
+  },
+
+  shiftInRow: function(indexOfClick, indexOfNull, rowOfClick) {
+    if (indexOfClick < indexOfNull) {
+      rowOfClick.splice(indexOfClick, 0, null);
+      rowOfClick.splice(indexOfNull + 1, 1);
+    } else if (indexOfClick > indexOfNull) {
+      rowOfClick.splice(indexOfNull, 1);
+      rowOfClick.splice(indexOfClick, 0, null);
+    }
+  },
+  shiftInColumn: function(rowId, colId, rowOfClick) {
+    //Loop through tiles until we find null
+    for (var rowWithNull = 0; rowWithNull < game.gameState.length; rowWithNull++) {
+      for (var colWithNull = 0; colWithNull < game.gameState[rowWithNull].length; colWithNull++) {
+        if (game.gameState[rowWithNull][colId] === null) {
+          //If we find null in a row after the clicked row
+          if (rowId < rowWithNull) {
+            var temp = rowOfClick[colId];
+            for (var k = rowId + 1; k <= rowWithNull; k++) {
+              var newTemp = game.gameState[k][colId];
+              game.gameState[k][colId] = temp;
+              temp = newTemp;
+            }
+            game.gameState[rowId][colId] = null;
+
+          //If we find null in a row before the row clicked
+        } else if (rowId > rowWithNull) {
+           var temp = rowOfClick[colId];
+          for (var k = rowId - 1; k >= rowWithNull; k--) {
+            var newTemp = game.gameState[k][colId];
+            game.gameState[k][colId] = temp;
+            temp = newTemp;
+          }
+          game.gameState[rowId][colId] = null;
+          }
+          break;
+        }
+      }
+    }
   }
 }
 
